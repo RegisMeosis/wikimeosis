93e674daf69233355afba730243d5bdb97470561
Victor-GASSMANN <vgassmann@meosis.fr>
2024-04-16 14:14:38 +0200

    intro

---
+++ src/pages/Intro.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <div>
+      <h1>Bienvenue sur notre site de documentation</h1>
+      <p>Sur ce site, vous trouverez des informations détaillées sur l'utilisation de notre produit.</p>
+      <h2>Sommaire :</h2>
+      <ul>
+        <li><a href="#elementor_sections">Les Sections avec Elementor</a></li>
+        <li><a href="#elementor_widget_accordeon">Widget Accordéon</a></li>
+        <li><a href="#elementor_autres_widgets">Autres Widgets</a></li>
+        <li><a href="#elementor_widget_boite_image">Widget Boîte d’image</a></li>
+        <li><a href="#elementor_widget_bouton">Widget Bouton</a></li>
+        <li><a href="#elementor_widget_galerie">Widget Galerie</a></li>
+        <li><a href="#elementor_widget_image">Widget Image</a></li>
+        <li><a href="#elementor_widget_liste_de_prix">Widget Liste de prix</a></li>
+        <li><a href="#elementor_widget_section_interne">Widget Section interne</a></li>
+        <li><a href="#elementor_widget_editeur_texte">Widget Éditeur de texte</a></li>
+        <li><a href="#elementor_widget_titre">Widget Titre</a></li>
+        <li><a href="#elementor_widget_video">Widget Vidéo</a></li>
+        <li><a href="#edition-popup">Édition de la Popup</a></li>
+        <li><a href="#gerer-modele-rapidement-popups">Gérer un modèle rapidement</a></li>
+        <li><a href="#programmation-popup">Programmation de la Popup</a></li>
+        <li><a href="#popups">Les Popups</a></li>
+        <li><a href="#actualites">L’onglet Actualités</a></li>
+        <li><a href="#avis">L’onglet Avis</a></li>
+        <li><a href="#tableaudebord">Le tableau de bord</a></li>
+        <li><a href="#medias">L’onglet Médias</a></li>
+        <li><a href="#l’onglet_menu">L’onglet Menu</a></li>
+        <li><a href="#messages">Onglet Messages</a></li>
+        <li><a href="#newsletter">Onglet Newsletter</a></li>
+        <li><a href="#pages">Onglet Pages</a></li>
+        <li><a href="#popups">Onglet Popups</a></li>
+        <li><a href="#statistiques">Onglet Statistiques</a></li>
+        <li><a href="#ajouter_les_actualites">Ajouter les Actualités</a></li>
+        <li><a href="#gerer_les_actualites">Gérer les Actualités</a></li>
+        <li><a href="#quest_ce_quune_actualite">Une actualité, c’est quoi ?</a></li>
+        <li><a href="#edition_de_l_actualite">Édition de l'Actualité</a></li>
+        <li><a href="#jerico4.0">Jerico 4.0</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Intro;
